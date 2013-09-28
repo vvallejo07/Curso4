3e49259a53a84759efd5d2990b02d1676e2be904
vvallejo07 <bk_soft@hotmail.com>
2013-09-28 16:58:44 -0500

    clase 4

---
+++ js/API/almacenamiento.js
@@ -0,0 +1,82 @@
+//Almacenamiento
+function altaUsario(){
+    window.localStorage.setItem('usuario',$('#regNom').val());
+}
+
+function usuarioExiste(){
+    if(window.localStorage.getItem('usuario')!=undefined)
+        return true;
+    else
+        return false;
+}
+
+function accesoBD(){
+    var db = window.openDatabase("hotel", "1.0", "Hotal BD", 2000000);
+    return db;
+}
+
+function guardarReserva(th,pr,ha,di){
+    accesoBD().transaction(function(tx){
+        tx.executeSql('CREATE TABLE IF NOT EXISTS reservas (id unique,th,pr,ha,di)');
+        tx.executeSql('INSERT INTO reservas (th,pr,ha,di) VALUES ("'+th+'","'+pr+'","'+ha+'","'+di+'")');
+    },function(err){
+        alert('Error: '+err.code);
+    },function(){
+        navigator.notification.alert('Esperando Conexión a Internet',null,'Datos Guardados','Aceptar');
+    });
+}
+
+function guardarHistorial(th,pr,ha,di){
+	accesoBD().transaction(function(tx){
+		tx.executeSql('CREATE TABLE IF NOT EXISTS historial (id unique,th,pr,ha,di)');
+		tx.executeSql('INSERT INTO historial (th,pr,ha,di) VALUES ("'+th+'","'+pr+'","'+ha+'","'+di+'")');
+	},function(err){
+		alert("Error: "+err.code);	
+	},function(){
+		navigator.notification.alert('Historial Guardado',null,'Historial','Aceptar');
+	});
+}
+
+function leerReservas(){
+	accesoBD().transaction(function(tx){
+		tx.executeSql('SELECT * FROM reservas',[],function(tx2,res){
+			var largo = res.rows.length;
+			for(i=0;i<largo;i++){
+				var th = res.rows.item(i).th;
+				var pr = res.rows.item(i).pr;
+				var ha = res.rows.item(i).ha;
+				var di = res.rows.item(i).di;
+				
+				alert(th+'\n'+pr+'\n'+ha+'\n'+di);
+			}
+		},function(err){
+			alert('Error: '+err.code);	
+		});
+	},function(err){
+		alert('Error: '+err.code);	
+	},function(){
+		var a;
+	});
+}
+
+function leerHistorial(){
+	accesoBD().transaction(function(tx){
+		tx.executeSql('SELECT * FROM historial',[],function(tx2,res){
+			var largo = res.rows.length;
+			for(i=0;i<largo;i++){
+				var th = res.rows.item(i).th;
+				var pr = res.rows.item(i).pr;
+				var ha = res.rows.item(i).ha;
+				var di = res.rows.item(i).di;
+				
+				alert(th+'\n'+pr+'\n'+ha+'\n'+di);
+			}
+		},function(err){
+			alert('Error: '+err.code);	
+		});
+	},function(err){
+		alert('Error: '+err.code);	
+	},function(){
+		var a;
+	});
+}
